a51f45372743e43e059b30030cb03c1d43fb96dc
Sarvesh Shivashankaraiah <pixelsaints@gmail.com>
2026-04-21 15:29:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
 <?php
 
-define('BASE_URL', 'http://multipaymart.local');
+if ($_SERVER['HTTP_HOST'] === 'multipaymart.local') {
+  define('BASE_URL', 'http://multipaymart.local');
+} else {
+  define('BASE_URL', 'https://nothingmatters.in/multipaymart');
+}
 
-$cssPath = $_SERVER['DOCUMENT_ROOT'] . '/assets/css/style.css';
+$cssPath = $_SERVER['DOCUMENT_ROOT'] . '/multipaymart/assets/css/style.css';
 $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time();
